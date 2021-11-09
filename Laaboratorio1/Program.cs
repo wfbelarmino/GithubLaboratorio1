597e0cdf8745bc5176647749673f739c0fc5d688
WagnerSouza <wsantsou@everis.com>
2021-11-09 11:04:56 -0300

    Add project files.

---
+++ Laaboratorio1/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Laaboratorio1
+        
+{
+    class Program
+    class Program2
+
+    {
+        static void Main(string[] args)
+        {
+            System.Console.WriteLine("Alô Mundo!");
+            System.Console.ReadLine();
+            
+        }
+    }
+}
